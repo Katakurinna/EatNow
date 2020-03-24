761c2988cecadf945d70c4939f416e488eddf849
Alejandro Cerrato Espejo <alesagustin@gmail.com>
2020-03-24 01:07:00 +0100

    Cambios varios.

---
+++ EatNow-ClientManager/src/java/me/cerratolabs/eatnow/clientmanager/view/login/LoginPanel.java
@@ -0,0 +1,158 @@
+package me.cerratolabs.eatnow.clientmanager.view.login;
+
+import javax.swing.*;
+import java.awt.Checkbox;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+public class LoginPanel extends JPanel {
+
+
+    public LoginPanel() {
+        initComponents();
+    }
+
+    private Checkbox acceptedTermsCheckBox;
+    private JPasswordField confirmPasswordField;
+    private JTextField emailField;
+    private JLabel jLabel1;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JButton loginButton;
+    private JTextField nameField;
+    private JPasswordField passwordField;
+    private JButton registerButton;
+
+    private void initComponents() {
+        GridBagConstraints gridBagConstraints;
+
+        jLabel1 = new JLabel();
+        jLabel3 = new JLabel();
+        jLabel4 = new JLabel();
+        jLabel5 = new JLabel();
+        confirmPasswordField = new JPasswordField();
+        passwordField = new JPasswordField();
+        emailField = new JTextField();
+        nameField = new JTextField();
+        registerButton = new JButton();
+        loginButton = new JButton();
+        acceptedTermsCheckBox = new Checkbox();
+
+        setPreferredSize(new Dimension(1115, 476));
+        setLayout(new GridBagLayout());
+
+        jLabel1.setText("Nombre:");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.ipadx = 74;
+        gridBagConstraints.ipady = 20;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(120, 378, 0, 0);
+        add(jLabel1, gridBagConstraints);
+
+        jLabel3.setText("Correo electronico:");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 23;
+        gridBagConstraints.ipady = 20;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(6, 378, 0, 0);
+        add(jLabel3, gridBagConstraints);
+
+        jLabel4.setText("Contraseña:");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipadx = 55;
+        gridBagConstraints.ipady = 20;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(6, 378, 0, 0);
+        add(jLabel4, gridBagConstraints);
+
+        jLabel5.setText("Confirma la contraseña:");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.ipady = 20;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(6, 378, 0, 0);
+        add(jLabel5, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.ipadx = 207;
+        gridBagConstraints.ipady = 14;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(6, 10, 0, 413);
+        add(confirmPasswordField, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.ipadx = 207;
+        gridBagConstraints.ipady = 14;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(6, 10, 0, 413);
+        add(passwordField, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.ipadx = 207;
+        gridBagConstraints.ipady = 14;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(6, 10, 0, 413);
+        add(emailField, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.ipadx = 207;
+        gridBagConstraints.ipady = 14;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(120, 10, 0, 413);
+        add(nameField, gridBagConstraints);
+
+        registerButton.setText("Registarme");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.ipadx = 25;
+        gridBagConstraints.ipady = 10;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(23, 10, 116, 0);
+        add(registerButton, gridBagConstraints);
+
+        loginButton.setText("Iniciar sesion");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.ipady = 10;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(23, 6, 116, 413);
+        add(loginButton, gridBagConstraints);
+
+        acceptedTermsCheckBox.setLabel("Acepto los terminos y condiciones");
+        acceptedTermsCheckBox.setState(true);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(10, 10, 0, 413);
+        add(acceptedTermsCheckBox, gridBagConstraints);
+        setVisible(true);
+
+    }
+
+}
