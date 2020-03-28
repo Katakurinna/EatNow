de8b61c54476816f7014cff2a7b79e27b213f056
Alejandro Cerrato Espejo <alesagustin@gmail.com>
2020-03-28 17:56:50 +0100

    Added ssha key for remote server deploy

--- Server/scripts/deploy.bat
+++ Server/scripts/deploy.bat
@@ -1,4 +1,4 @@
 :: EatNow deploy file for windows clients.
 cd target
-echo put server-1.0.jar | sftp katakurinna@192.168.0.30:server
-echo ./server-restart-script.sh | ssh katakurinna@192.168.0.30
+echo put server-1.0.jar | sftp -i ~/.ssh/virtualbox_unix_key katakurinna@192.168.0.30:server
+echo ./server-restart-script.sh | ssh -i ~/.ssh/virtualbox_unix_key katakurinna@192.168.0.30
