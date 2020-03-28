2fb6a75a9f38ce5c7dcfad2f2ff429310bcaf1e4
Alejandro Cerrato Espejo <alesagustin@gmail.com>
2020-03-28 20:00:00 +0100

    Deleted unused server address field from 'ModelConstants'

--- Server/src/main/java/me/cerratolabs/eatnow/server/model/ModelConstants.java
+++ Server/src/main/java/me/cerratolabs/eatnow/server/model/ModelConstants.java
@@ -1,15 +1,14 @@
 package me.cerratolabs.eatnow.server.model;
 
 public class ModelConstants {
+
     // Paths
     public static final String CONFIG_FILE_PATH = "src\\resources\\configFiles\\configuration.conf";
     public static final String TOKEN_FILE_PATH = "src\\resources\\configFiles\\token.dat";
 
     // Properties file keys
-    public static final String SERVER_ADDRESS_NAME = "ServerAddress";
     public static final String SERVER_PORT_NAME = "ServerPort";
 
     // Properties file value
-    public static final String SERVER_ADDRESS_DEFAULT_VALUE = "192.168.0.30";
     public static final String SERVER_PORT_DEFAULT_VALUE = "3121";
 }
